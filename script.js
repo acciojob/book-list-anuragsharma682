b704110c7f29fdb59f128e1107a50dc95ce446c9
Anurag Sharma <162544675+anuragsharma682@users.noreply.github.com>
2025-12-27 22:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+document.getElementById("book-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const isbn = document.getElementById("isbn").value;
+
+    const tableBody = document.getElementById("book-list");
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${title}</td>
+        <td>${author}</td>
+        <td>${isbn}</td>
+        <td><button class="delete">Clear</button></td>
+    `;
+
+    tableBody.appendChild(row);
+
+    document.getElementById("book-form").reset();
+});
+
+document.getElementById("book-list").addEventListener("click", function (e) {
+    if (e.target.classList.contains("delete")) {
+        e.target.parentElement.parentElement.remove();
+    }
+});
